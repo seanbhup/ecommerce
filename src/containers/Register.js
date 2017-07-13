827658cb760f187befb0abbcb4ed34ae3ebaf3a0
Robert Bunch <rob@digitalcrafts.com>
2017-07-13 12:09:13 -0400

    Add register container and action

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,13 +1,111 @@
 import React, {Component} from 'react';
+import { Form, FormGroup, ControlLabel, FormControl, Button, Col ,MenuItem} from 'react-bootstrap'
+// Our action needs bindActionCreators from redux
+import  {bindActionCreators} from 'redux';
+// Get the registerAction function which runs on submission
+import RegisterAction from '../actions/RegisterAction';
+// Because this is a container, we need connect from react-redux!
+import {connect} from 'react-redux';
+
 
 class Register extends Component{
+	constructor(props) {
+		super(props);
+		this.handleRegistration = this.handleRegistration.bind(this);
+	}
+
+	handleRegistration(event){
+		event.preventDefault();
+		console.log("User SUbmitted the form!!")
+		var name = event.target[0].value
+		var email = event.target[1].value
+		var accountType = "customer"
+		var password = event.target[3].value
+		var city = event.target[4].value
+		var state = event.target[5].value
+		var salesRep = event.target[6].value
+		console.log(name);
+		this.props.registerAction();
+	}
+
 	render(){
 		return(
-			<div>
-				<h1>Register Page</h1>
+			<div className="register-wrapper">
+				<Form horizontal onSubmit={this.handleRegistration}>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Name
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="fullName" placeholder="Full Name" />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Email
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="email" placeholder="Email" />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Account Type
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="type" value="customer" disabled />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Password
+						</Col>
+						<Col sm={10}>
+							<FormControl type="password" name="password" placeholder="Password" />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							City
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="city" placeholder="City" />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							State
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="state" placeholder="State" />
+						</Col>
+					</FormGroup>
+					<FormGroup controlId="formHorizontalName">
+						<Col componentClass={ControlLabel} sm={2}>
+							Sales Rep
+						</Col>
+						<Col sm={10}>
+							<FormControl type="text" name="employee" placeholder="Employee you worked with" />
+						</Col>
+					</FormGroup>
+					<FormGroup>
+						<Col smOffset={2} sm={10}>
+							<Button bsStyle="primary" bsSize="small" type="submit">
+								Register
+							</Button>
+						</Col>
+					</FormGroup>
+				</Form>
 			</div>
 		)
 	}
 }
 
-export default Register;
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({
+		registerAction: RegisterAction
+	}, dispatch)
+}
+
+// export default Register;
+export default connect(null,mapDispatchToProps)(Register);
